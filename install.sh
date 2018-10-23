0881486c39ecbc59ed44354aea21345d10862816
drinks <me@mum5.cn>
2018-10-23 16:39:29 +0800

    second commit

--- install.sh
+++ install.sh
@@ -7,8 +7,19 @@ apt-get update && apt-get -y install git
 git clone https://github.com/flyzy2005/ss-fly ~/ss-fly
 
 #
+CONFIG ='{
+    "server":"0.0.0.0",
+    "server_port":443,
+    "local_address": "127.0.0.1",
+    "local_port":1080,
+    "password":"1qaz2wsx3edcss",
+    "timeout":300,
+    "method":"bf-cfb",
+    "fast_open": true,
+    "workers": 1,
+    "prefer_ipv6":true 
+}'
+
 mkdir -p /etc/shadowsocks
-cp config /etc/shadowsocks
-
+echo $CONFIG > /etc/shadowsocks/config.json
 ~/ss-fly/ss-fly.sh
-
