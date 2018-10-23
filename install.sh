130510e24b1de73adc84ce1972712a6a172276f2
drinks <me@mum5.cn>
2018-10-23 17:06:46 +0800

    commit

--- install.sh
+++ install.sh
@@ -1,28 +1,10 @@
 #!/bin/bash
 
-# install bbr
-# wget --no-check-certificate https://github.com/teddysun/across/raw/master/bbr.sh && chmod +x bbr.sh && ./bbr.sh
-
 apt-get update && apt-get -y install git
 git clone https://github.com/flyzy2005/ss-fly ~/ss-fly
-
-#
-CONFIG ='{
-    "server":"0.0.0.0",
-    "server_port":443,
-    "local_address": "127.0.0.1",
-    "local_port":1080,
-    "password":"1qaz2wsx3edcss",
-    "timeout":300,
-    "method":"bf-cfb",
-    "fast_open": true,
-    "workers": 1,
-    "prefer_ipv6":true 
-}'
-
-mkdir -p /etc/shadowsocks
-echo $CONFIG > /etc/shadowsocks/config.json
 ~/ss-fly/ss-fly.sh -bbr
-~/ss-fly/ss-fly.sh -i password 443
-
-echh $CONFIG > /etc/shadowsocks/shadowsocks.json
+read -t 30 -s -p "请输入密码:" PASSWD
+echo -e "\n"
+~/ss-fly/ss-fly.sh -i $PASSWD 443
+sed -i "s/aes-256-cfb/bf-cfb/g" /etc/shadowsocks.json
+~/ss-fly/ss-fly restart
