572570e3be21c3501d01c4f105d600ee4b8e73c2
drinks <me@mum5.cn>
2018-10-23 17:14:01 +0800

    commit

--- install.sh
+++ install.sh
@@ -6,5 +6,6 @@ git clone https://github.com/flyzy2005/ss-fly ~/ss-fly
 read -t 30 -s -p "请输入密码:" PASSWD
 echo -e "\n"
 ~/ss-fly/ss-fly.sh -i $PASSWD 443
+echo "正在将加密方式修改为bf-cfb"
 sed -i "s/aes-256-cfb/bf-cfb/g" /etc/shadowsocks.json
 ~/ss-fly/ss-fly restart
